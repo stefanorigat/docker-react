28892f5318c9163008f77ea85c2f11e0703b5645
Stefano Rigat <stefano.rigat@deltatre.it>
2021-12-31 18:42:44 +0100

    changed app title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          Hi there! I was changed in the Feature Branch!
         </p>
         <a
           className="App-link"
